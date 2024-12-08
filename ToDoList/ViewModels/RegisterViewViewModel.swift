57e6b30a63c48cbf735a1aef867c590d7d9d0822
Yvan Gauthier <ygauthier81@icloud.com>
2024-12-08 15:20:54 +0100

    Initial Commit

---
+++ ToDoList/ViewModels/RegisterViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewViewModel.swift
+//  ToDoList
+//
+//  Created by Yvan Gauthier on 08/12/2024.
+//
+
+import Foundation
---
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoListItemViewViewModel.swift
+//  ToDoList
+//
+//  Created by Yvan Gauthier on 08/12/2024.
+//
+
+import Foundation
